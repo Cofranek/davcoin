679200507477f5c1a77f3833da1f326a501bc50d
Cofranek <162723835+Cofranek@users.noreply.github.com>
2024-03-08 16:16:26 +0100

    Create modmain.js

---
+++ modmain.js
@@ -0,0 +1,6 @@
+document.getElementById('head').textContent = "DavCoin - " + "your mod name"
+
+function set() { /* Declaring the function */
+    max = -1 /* Setting the max value change to 1 */
+}
+setInterval(set, 500) /* Repeat */
